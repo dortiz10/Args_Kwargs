8cb2f579a966dcf9744c877e9fa422247bfc273a
David Ortiz <113551897+dortiz10@users.noreply.github.com>
2022-11-10 20:01:53 +0000

    Args

---
+++ main.py
@@ -0,0 +1,11 @@
+def my_sum(*args):
+  print(args)
+print(my_sum(3,4,5))
+#args are for entering as many arguments as possible
+def a_sum (*args):
+  for num in args:
+    print(num)
+
+print(a_sum(1))
+print(a_sum(1,3))
+print(a_sum(1,3,4,5,6))
